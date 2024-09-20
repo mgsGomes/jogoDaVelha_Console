625d3f80a0626333e1fa30956e3b9e5e6d678792
mgsGomes <micaelmgs@gmail.com>
2024-09-20 00:41:15 -0300

    Checking inicial.

---
+++ JogoDaVelha/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JogoDaVelha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
